7b885f641bf8e075426284ea83769218c4d36972
franfreezy <frandelwanjawa@yahoo.com>
2024-01-02 23:23:41 +0300

    keypad module done with

--- jujaHome/src/main.cpp
+++ jujaHome/src/main.cpp
@@ -4,17 +4,33 @@
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
-//define the cymbols on the buttons of the keypads
+
+
+//we create 2 arrays one with the password and the other, empty
+
+int i=0;
+
+#define pass_length  5
+
+char passcode[pass_length] ="1234";
+
+char confirmPasscode[pass_length] ;
+
+
+
+//define the symbols on the buttons of the keypads
 char hexaKeys[ROWS][COLS] = {
-  {'1','4','7','f'},
+  {'1','4','7','*'},
   {'2','5','8','0'},
   {'3','6','9','#'},
   {'A','B','C','D'}
 };
+
+//definition of pins
 byte rowPins[ROWS] = {53, 52, 51, 50}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {49, 48, 47, 46}; //connect to the column pinouts of the keypad
 
-//initialize an instance of class NewKeypad
+//initialize an instance of class NewKeypad, this comes with the library
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
 void setup(){
@@ -22,9 +38,23 @@ void setup(){
 }
   
 void loop(){
-  char customKey = customKeypad.getKey();
+   char customKey = customKeypad.getKey();
   
   if (customKey){
-    Serial.println(customKey);
+    confirmPasscode[i]=customKey;
+    i++;
+   
   }
+  if(i == pass_length-1)
+  {
+      if(!strcmp(passcode,confirmPasscode))
+      {
+  
+      Serial.println("correct");
+       Serial.println(confirmPasscode);
+       }
+    }
 }
+  
+   
+  
