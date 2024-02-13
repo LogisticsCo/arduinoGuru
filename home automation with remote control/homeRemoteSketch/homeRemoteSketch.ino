b76aab6ad0176796f42642ca43a72c7d55ebaa9c
franfreezy <frandelwanjawa@yahoo.com>
2024-02-13 14:47:26 +0300

    additional

--- home automation with remote control/homeRemoteSketch/homeRemoteSketch.ino
+++ home automation with remote control/homeRemoteSketch/homeRemoteSketch.ino
@@ -1,5 +1,6 @@
 #include <IRremote.h>
-IRrecv IR(13);
+IRrecv IR(12);
+int led=13;
 int office=22;
 int kitchen=24;
 int lappy=26;
@@ -25,50 +26,69 @@ while (inputpasscode!=passcode){
       IR.resume();
     }
     else{
-      Serial.println("hehre");
-      Serial.println(count);
+      
         if(count<4){
 
             if(IR.decodedIRData.decodedRawData==0xFE017F00){
             //1
-
+                digitalWrite(led,HIGH);
+                delay(500);
+                digitalWrite(led,LOW);
                 inputpasscode+=1;
                 Serial.println(inputpasscode);
                 count+=1;
 
             }
             if(IR.decodedIRData.decodedRawData==0xFD027F00){
-            //2
+            //2 
+                digitalWrite(led,HIGH);
+                delay(500);
+                digitalWrite(led,LOW);
                 inputpasscode+="2";
                 Serial.println(inputpasscode);
                 count+=1;
             }
             if(IR.decodedIRData.decodedRawData==0xFC037F00){
             //3
+                digitalWrite(led,HIGH);
+                delay(500);
+                digitalWrite(led,LOW);
                 inputpasscode+="3";
                 Serial.println(inputpasscode);
                 count+=1;
             }
             if(IR.decodedIRData.decodedRawData==0xFB047F00){
             //4
+                digitalWrite(led,HIGH);
+                delay(500);
+                digitalWrite(led,LOW);
                 inputpasscode+="4";
                 Serial.println(inputpasscode);
                 count+=1;
             }
             if(IR.decodedIRData.decodedRawData==0xFA057F00){
-            //5
+            //5 
+                digitalWrite(led,HIGH);
+                delay(500);
+                digitalWrite(led,LOW);
                 inputpasscode+="5";
                 Serial.println(inputpasscode);
                 count+=1;
             }
             if(IR.decodedIRData.decodedRawData==0xF9067F00){
             //6
+                digitalWrite(led,HIGH);
+                delay(500);
+                digitalWrite(led,LOW);
                 inputpasscode+="6";
                 Serial.println(inputpasscode);
                 count+=1;
             }
             if(IR.decodedIRData.decodedRawData==0xF8077F00){
             //7
+                digitalWrite(led,HIGH);
+                delay(500);
+                digitalWrite(led,LOW);
                 inputpasscode+="7";
                 Serial.println(inputpasscode);
                 count+=1;
@@ -76,12 +96,18 @@ while (inputpasscode!=passcode){
 
             if(IR.decodedIRData.decodedRawData==0xF7087F00){
             //8
+                digitalWrite(led,HIGH);
+                delay(500);
+                digitalWrite(led,LOW);
                 inputpasscode+="8";
                 Serial.println(inputpasscode);
                 count+=1;
             }
             if(IR.decodedIRData.decodedRawData==0xF6097F00){
             //9
+                digitalWrite(led,HIGH);
+                delay(500);
+                digitalWrite(led,LOW);
                 inputpasscode+="9";
                 Serial.println(inputpasscode);
                 count+=1;
@@ -89,7 +115,7 @@ while (inputpasscode!=passcode){
 
 
         }
-        if(count==3 && passcode!=inputpasscode){
+        if(count==4 && passcode!=inputpasscode){
         Serial.println("incorrect");
         count=0;
         inputpasscode="";
@@ -114,7 +140,10 @@ digitalWrite(office,LOW);
 void loop() {
   if(IR.decode()){
     Serial.println(IR.decodedIRData.decodedRawData, HEX);
-  
+    if(IR.decodedIRData.decodedRawData==0x0){
+      
+      IR.resume();
+    }
     if(IR.decodedIRData.decodedRawData == 0xEA157F00){
       //OFF  
       digitalWrite(office,LOW);
