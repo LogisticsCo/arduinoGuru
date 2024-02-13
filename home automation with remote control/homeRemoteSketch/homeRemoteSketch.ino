383942e1aa050ffc3330ab90416a16836af7ab41
franfreezy <frandelwanjawa@yahoo.com>
2024-02-13 13:24:00 +0300

    addititional

--- home automation with remote control/homeRemoteSketch/homeRemoteSketch.ino
+++ home automation with remote control/homeRemoteSketch/homeRemoteSketch.ino
@@ -4,8 +4,9 @@ int office=22;
 int kitchen=24;
 int lappy=26;
 int tv= 28;
-
-
+int count=0;
+char passcode ='1234';
+char inputpasscode=' ';
 void setup() {
 IR.enableIRIn();
 pinMode(office,OUTPUT);
@@ -13,9 +14,15 @@ pinMode(kitchen,OUTPUT);
 pinMode(lappy,OUTPUT);
 pinMode(tv,OUTPUT);
 Serial.begin(9600);
-count=0;
+
+//put a passcode-4 digit
 while (count<4){
-  Serial.println("starting up");
+  Serial.print(count);
+  if(IR.decode()){
+    count+=1;
+    Serial.println(IR.decodedIRData.decodedRawData, HEX);
+  }
+  
 }
 
 
@@ -24,7 +31,8 @@ while (count<4){
 void loop() {
   if(IR.decode()){
     Serial.println(IR.decodedIRData.decodedRawData, HEX);
-    if(IR.decodedIRData.decodedRawData==OxEA157FOO){
+  
+    if(IR.decodedIRData.decodedRawData == 0xEA157F00){
       //OFF  
       digitalWrite(office,LOW);
       digitalWrite(tv,LOW);
@@ -33,49 +41,49 @@ void loop() {
 
 
     }
-    if(IR.decodedIRData.decodedRawData==OxFE017FOO){
+    if(IR.decodedIRData.decodedRawData==0xFE017F00){
     //1
     digitalWrite(office,HIGH);
 
     }
-    if(IR.decodedIRData.decodedRawData==OxFD027FOO){
+    if(IR.decodedIRData.decodedRawData==0xFD027F00){
     //2
     digitalWrite(office,LOW);
     }
-    if(IR.decodedIRData.decodedRawData==OxFC037FOO){
+    if(IR.decodedIRData.decodedRawData==0xFC037F00){
     //3
     digitalWrite(tv,HIGH);
     }
-    if(IR.decodedIRData.decodedRawData==OxFB047FOO){
+    if(IR.decodedIRData.decodedRawData==0xFB047F00){
     //4
     digitalWrite(tv,LOW);
     }
-    if(IR.decodedIRData.decodedRawData==OxFA057FOO){
+    if(IR.decodedIRData.decodedRawData==0xFA057F00){
     //5
     digitalWrite(lappy,HIGH);
     }
-    if(IR.decodedIRData.decodedRawData==OxF9067FOO){
+    if(IR.decodedIRData.decodedRawData==0xF9067F00){
     //6
     digitalWrite(lappy,LOW);
     }
-    if(IR.decodedIRData.decodedRawData==OxF8077FOO){
+    if(IR.decodedIRData.decodedRawData==0xF8077F00){
     //7
     digitalWrite(kitchen,HIGH);
     }
-    if(IR.decodedIRData.decodedRawData==OxEF107FOO){
+    if(IR.decodedIRData.decodedRawData==0xEF107F00){
     //ON
     digitalWrite(office,HIGH);
     digitalWrite(tv,HIGH);
     digitalWrite(lappy,HIGH);
     digitalWrite(kitchen,HIGH);
     }
-    if(IR.decodedIRData.decodedRawData==OxF7087FOO){
+    if(IR.decodedIRData.decodedRawData==0xF7087F00){
     //8
     digitalWrite(kitchen,LOW);
     }
-    if(IR.decodedIRData.decodedRawData==OxF6097FOO){
+    if(IR.decodedIRData.decodedRawData==0xF6097F00){
     //9
-    digitalWrite()
+    //
     }
   delay(1000);
   IR.resume();
