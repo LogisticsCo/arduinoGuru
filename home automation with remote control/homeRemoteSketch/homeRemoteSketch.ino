31812385b2e51eb40a26316728691b34000f1701
franfreezy <frandelwanjawa@yahoo.com>
2024-02-13 08:48:44 +0300

    CODES DECODED

--- home automation with remote control/homeRemoteSketch/homeRemoteSketch.ino
+++ home automation with remote control/homeRemoteSketch/homeRemoteSketch.ino
@@ -11,29 +11,38 @@ Serial.begin(9600);
 void loop() {
   if(IR.decode()){
     Serial.println(IR.decodedIRData.decodedRawData, HEX);
-    if(IR.decodedIRData.decodedRawData==Ox){
-
+    if(IR.decodedIRData.decodedRawData==OxEA157FOO){
+//ON
     }
-    if(IR.decodedIRData.decodedRawData==Ox){
-
+    if(IR.decodedIRData.decodedRawData==OxFE017FOO){
+//1
     }
-    if(IR.decodedIRData.decodedRawData==Ox){
-
+    if(IR.decodedIRData.decodedRawData==OxFD027FOO){
+//2
     }
-    if(IR.decodedIRData.decodedRawData==Ox){
-
+    if(IR.decodedIRData.decodedRawData==OxFC037FOO){
+//3
     }
-    if(IR.decodedIRData.decodedRawData==Ox){
-
+    if(IR.decodedIRData.decodedRawData==OxFB047FOO){
+//4
     }
-    if(IR.decodedIRData.decodedRawData==Ox){
-
+    if(IR.decodedIRData.decodedRawData==OxFA057FOO){
+//5
     }
-    if(IR.decodedIRData.decodedRawData==Ox){
-
+    if(IR.decodedIRData.decodedRawData==OxF9067FOO){
+//6
     }
-    if(IR.decodedIRData.decodedRawData==Ox){
-
+    if(IR.decodedIRData.decodedRawData==OxF8077FOO){
+//7
+    }
+    if(IR.decodedIRData.decodedRawData==OxEF107FOO){
+//OFF
+    }
+    if(IR.decodedIRData.decodedRawData==OxF7087FOO){
+//8
+    }
+    if(IR.decodedIRData.decodedRawData==OxF6097FOO){
+//9
     }
   delay(2000);
   IR.resume();
